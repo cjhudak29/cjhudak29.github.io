5dffdc595ac0006862662528c19a624a6772a5c0
cjhudak29 <50032574+cjhudak29@users.noreply.github.com>
2021-06-04 16:05:38 -0400

    Add files via upload

---
+++ scripts/formvalidation.js
@@ -0,0 +1,42 @@
+function validateForm(){
+	/* Declare Variables */
+	var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var emailAddress = document.getElementById('emailAddress');
+    var letters = /^[A-Za-z]+$/;
+	
+	if (firstName.value.length <= 2) {
+        /* triggered if the length of firstName is two characters or less */
+        alert('Sorry: First name must contain two (2) or more characters.');
+        changeIt();
+        firstName.focus();
+        return false;
+    } else if (!firstName.value.match(letters)) {
+        /* Triggered if firstName contains special characters or numbers */
+        alert('Sorry: First name can only contain letters A-Z.');
+        changeIt();
+        firstName.focus();
+        return false;
+    } else if (lastName.value.length <= 2) {
+        /* Triggered if the length of lastName is two characters or less */
+        alert('Sorry: Last name must contain two (2) or more characters.');
+        changeIt();
+        lastName.focus();
+        return false;
+    } else if (!lastName.value.match(letters)) {
+        /* Triggered if lastName contains special characters or numbers */
+        alert('Sorry: Last name can only contain letters A-Z.');
+        changeIt();
+        lastName.focus();
+        return false;
+    }
+}
+
+function changeIt() {
+    document.getElementById('errorMessage').innerHTML = 'Please review all errors and try again!';
+}
+
+function changeItTrue() {
+    document.getElementById('errorMessage').innerHTML = 'Please review all errors and try again!';
+}
+
