36ed72708fa3d18dfc27431c5c519b5300d9aa6d
cjhudak29 <cjhudak29@gmail.com>
2021-06-20 21:30:48 -0400

    Formatting Changes

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,17 +1,18 @@
 setInterval(showTime, 1000);
+
 function showTime() {
     var d = new Date();
     var h = d.getHours();
-	var message = "<br>Good Morning, World!";
-	
-	if(h >= 0 && h <12){
-		message="Good Morning, World!";
-	} else if(h>=12 && h<=18){
-		message="Good Afternoon, World!";
-	} else{
-		message = "Good Evening, World!";
-	}
+    var message = "<br>Good Morning, World!";
+
+    if (h >= 0 && h < 12) {
+        message = "Good Morning, World!";
+    } else if (h >= 12 && h < 15) {
+        message = "Good Afternoon, World!";
+    } else {
+        message = "Good Evening, World!";
+    }
     document.getElementById("greeting")
-            .innerHTML = message;
+        .innerHTML = message;
 }
 showTime();
